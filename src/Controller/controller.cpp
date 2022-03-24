95664a67cb5f46221906d117362165c761274f78
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-24 22:02:35 +0300

    Add getters and setters
    Closes #20

--- src/Controller/controller.cpp
+++ src/Controller/controller.cpp
@@ -1,1 +1,13 @@
 #include "controller.h"
+
+const std::vector<Polygon>& Controller::Polygons() const {
+  return polygons_;
+}
+
+const QPointF& Controller::LightSource() const {
+  return light_source_;
+}
+
+void Controller::SetLightSource(const QPointF& light_source) {
+  light_source_ = light_source;
+}
--- src/Controller/controller.h
+++ src/Controller/controller.h
@@ -8,6 +8,11 @@ class Controller {
  public:
   Controller() = default;
 
+  const std::vector<Polygon>& Polygons() const;
+
+  const QPointF& LightSource() const;
+  void SetLightSource(const QPointF& light_source);
+
  private:
   std::vector<Polygon> polygons_;
   QPointF light_source_;
