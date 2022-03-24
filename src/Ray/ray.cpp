049b2797201f479e267be7d0f5872c24e7249ed0
Andrei <67242525+DudkoAndrei@users.noreply.github.com>
2022-03-24 15:45:58 +0300

    Add Ray class (#9)
    * Add Ray class and constructors
    
    Closes #1
    
    * Add getters
    
    Closes #2
    
    * Add setters
    
    Closes #3
    
    * Add Rotate method
    
    Closes #4
    
    * Move from C++17 standard to C++20
    
    * Replace some trigonometric functions with std::numbers::pi
    
    * Add comparison operator
    
    Closes #8
    
    * Install g++ 11 in "Build" workflow

---
+++ src/Ray/ray.cpp
@@ -0,0 +1,86 @@
+#include "ray.h"
+
+#include <cmath>
+#include <numbers>
+
+Ray::Ray(const QPointF& begin, const QPointF& end)
+    : begin_(begin), direction_(end - begin), angle_(GetAngle(direction_)) {}
+
+Ray::Ray(const QPointF& begin, double angle)
+    : begin_(begin), angle_(angle),
+      direction_(std::cos(angle), std::sin(angle)) {}
+
+double Ray::GetAngle(const QPointF& point) {
+  double angle;
+
+  if (point.x() == 0) {
+    if (point.y() > 0) {
+      angle = std::numbers::pi / 2.0;
+    } else {
+      angle = -std::numbers::pi / 2.0;
+    }
+  } else {
+    angle = std::atan(point.y() / point.x());
+  }
+
+  if (angle < 0) {
+    angle += 2.0 * std::numbers::pi;
+  }
+
+  return angle;
+}
+
+const QPointF& Ray::Begin() const {
+  return begin_;
+}
+
+const QPointF& Ray::Direction() const {
+  return direction_;
+}
+
+QPointF Ray::End() const {
+  return begin_ + direction_;
+}
+
+double Ray::Angle() const {
+  return angle_;
+}
+
+void Ray::SetBegin(const QPointF& point) {
+  begin_ = point;
+}
+
+void Ray::SetDirection(const QPointF& point) {
+  direction_ = point;
+  angle_ = GetAngle(point);
+}
+
+void Ray::SetEnd(const QPointF& point) {
+  direction_ = point - begin_;
+  angle_ = GetAngle(direction_);
+}
+
+void Ray::SetAngle(double angle) {
+  angle_ = angle;
+  double length = std::sqrt(
+      direction_.x() * direction_.x() + direction_.y() * direction_.y());
+  direction_ = QPointF(std::cos(angle_), std::sin(angle_)) * length;
+}
+
+Ray Ray::Rotate(double angle) const {
+  Ray result = *this;
+
+  double sin = std::sin(angle);
+  double cos = std::cos(angle);
+
+  result.SetDirection({cos * result.direction_.x()
+                           - sin * result.direction_.y(),
+                       sin * result.direction_.x()
+                           + cos * result.direction_.y()});
+
+  return result;
+}
+
+auto Ray::operator<=>(const Ray& rhs) const {
+  return angle_ <=> rhs.angle_;
+}
---
+++ src/Ray/ray.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <compare>
+#include <QPointF>
+
+class Ray {
+ public:
+  Ray() = default;
+  Ray(const QPointF& begin, const QPointF& end);
+  Ray(const QPointF& begin, double angle);
+
+  const QPointF& Begin() const;
+  const QPointF& Direction() const;
+  QPointF End() const;
+  double Angle() const;
+
+  void SetBegin(const QPointF& point);
+  void SetDirection(const QPointF& point);
+  void SetEnd(const QPointF& point);
+  void SetAngle(double angle);
+
+  Ray Rotate(double angle) const;
+
+  auto operator<=>(const Ray& rhs) const;
+
+ private:
+  static double GetAngle(const QPointF& point);
+
+  QPointF begin_{0, 0};
+  QPointF direction_{0, 0};
+  double angle_{0};
+};
