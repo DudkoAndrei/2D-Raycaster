de90c7dd5085b3be9534ec1f905ec0e9531a7a13
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-26 00:35:58 +0300

    Refactor MainWindow::ConnectWidgets

--- src/MainWindow/main_window.cpp
+++ src/MainWindow/main_window.cpp
@@ -11,9 +11,8 @@ MainWindow::MainWindow()
   PlaceWidgets();
   ConnectWidgets();
 
-  mode_selector_->addItem("Light");
   mode_selector_->addItem("Polygons");
-  mode_selector_->setCurrentIndex(1);
+  mode_selector_->addItem("Light");
 
   setFixedSize(screen()->availableSize() * 0.8);
 
@@ -30,14 +29,19 @@ void MainWindow::PlaceWidgets() {
 void MainWindow::ConnectWidgets() {
   connect(mode_selector_, &QComboBox::currentIndexChanged, [&](int index) {
     mode_ = static_cast<Mode>(index);
-
-    points_.clear();
   });
 
   connect(paint_widget_, &PaintWidget::MouseLeftClicked, [&](
       const QPointF& point) {
     if (mode_ == Mode::kPolygons) {
-      points_.push_back(point);
+      if (controller_.Polygons().empty()) {
+        InitializeController();
+        controller_.AddPolygon({});
+      }
+      controller_.AddVertexToLastPolygon(point);
+      light_areas_ = controller_.CreateLightAreas();
+
+      repaint();
     }
   });
 
@@ -47,16 +51,15 @@ void MainWindow::ConnectWidgets() {
       if (controller_.Polygons().empty()) {
         InitializeController();
       }
-      if (!points_.empty()) {
-        controller_.AddPolygon(Polygon(std::move(points_)));
-        points_.clear();
-
-        repaint();
-      }
+      controller_.AddPolygon({});
     }
   });
 
   connect(paint_widget_, &PaintWidget::MouseMoved, [&](const QPointF& point) {
+    if (controller_.Polygons().empty()) {
+      InitializeController();
+      controller_.AddPolygon({});
+    }
     if (mode_ == Mode::kLight) {
       controller_.SetLightSource(point);
       light_areas_ = controller_.CreateLightAreas();
