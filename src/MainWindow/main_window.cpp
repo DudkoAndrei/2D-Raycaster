35c0d1d79adf5d6d2e7c45e350129dedc668c6fa
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-25 16:03:38 +0300

    Add necessary widgets

--- src/MainWindow/main_window.cpp
+++ src/MainWindow/main_window.cpp
@@ -1,3 +1,25 @@
 #include "main_window.h"
 
-MainWindow::MainWindow() {}
+MainWindow::MainWindow() :
+    widget_(new QWidget(this)),
+    layout_(new QGridLayout(widget_)),
+    mode_selector_(new QComboBox(widget_)),
+    scene_(new QGraphicsScene(widget_)),
+    view_(new QGraphicsView(scene_, widget_)) {
+  resize(1000, 800);
+
+  PlaceWidgets();
+  ConnectWidgets();
+
+  setCentralWidget(widget_);
+}
+
+void MainWindow::PlaceWidgets() {
+  layout_->addWidget(view_, 0, 0, 1, 2);
+  layout_->addWidget(mode_selector_, 1, 0, 1, 1);
+
+  widget_->setLayout(layout_);
+}
+
+void MainWindow::ConnectWidgets() {
+}
--- src/MainWindow/main_window.h
+++ src/MainWindow/main_window.h
@@ -1,11 +1,40 @@
 #pragma once
 
+#include <QComboBox>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGridLayout>
 #include <QMainWindow>
+#include <QPixmap>
+
+#include "../Controller/controller.h"
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
+
  public:
   MainWindow();
 
  private:
+  void PlaceWidgets();
+  void ConnectWidgets();
+
+  enum class Mode{
+    kLight,
+    kPolygons
+  };
+
+  Mode mode_;
+
+  QWidget* widget_;
+  QGridLayout* layout_;
+
+  QComboBox* mode_selector_;
+
+  QPixmap pixmap_{800, 600};
+
+  QGraphicsScene* scene_;
+  QGraphicsView* view_;
+
+  Controller controller_;
 };
