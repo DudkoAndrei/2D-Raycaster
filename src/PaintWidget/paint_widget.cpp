719981b4389d5520e04b68641f6427cd5ca74f92
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-25 21:15:43 +0300

    Implement mouse events

--- src/PaintWidget/paint_widget.cpp
+++ src/PaintWidget/paint_widget.cpp
@@ -3,3 +3,27 @@
 PaintWidget::PaintWidget(QWidget* parent) : QWidget(parent) {
   setMouseTracking(true);
 }
+
+void PaintWidget::mouseMoveEvent(QMouseEvent* event) {
+  QWidget::mouseMoveEvent(event);
+
+  emit MouseMoved(event->position());
+}
+
+void PaintWidget::mousePressEvent(QMouseEvent* event) {
+  QWidget::mousePressEvent(event);
+
+  switch (event->button()) {
+    case Qt::LeftButton: {
+      emit MouseLeftClicked(event->position());
+
+      break;
+    }
+    case Qt::RightButton: {
+      emit MouseRightClicked(event->position());
+
+      break;
+    }
+    default: {}
+  }
+}
