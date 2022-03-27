2a079b7f88b2fbca3c5d1496b3fc4a3e2d702839
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-27 16:18:04 +0300

    Fix Controller::RemoveAdjacentPoints method

--- src/Controller/controller.cpp
+++ src/Controller/controller.cpp
@@ -101,12 +101,11 @@ double Controller::GetLineLength(const QPointF& a, const QPointF& b) {
 void Controller::RemoveAdjacentPoints(
     std::vector<std::vector<QPointF>>* points) {
   for (size_t i = 0; i < points->size(); ++i) {
-    if (!points[i].empty()) {
-      for (size_t j = points->operator[](i).size() - 1; j > 0; --j) {
-        if (GetLineLength(points->operator[](i)[j],
-                          points->operator[](i)[j - 1]) < 1e-9) {
-          points->operator[](i).pop_back();
-        }
+    for (size_t j = 0; j < (*points)[i].size() - 1 && !(*points)[i].empty();
+         ++j) {
+      if (GetLineLength((*points)[i][j], (*points)[i][j - 1]) < 1e-9) {
+        (*points)[i].erase((*points)[i].begin() + j);
+        --j;
       }
     }
   }
